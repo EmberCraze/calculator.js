a4f509a6165a027fd0b7f1cf7b56b1d223849f9c
embercraze <maher.shaker@live.se>
2023-09-17 10:56:18 +0200

    feat: add post method and expose that to the number 1 button and delete button

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,14 +1,28 @@
-import { GetScreenData } from './requests';
+import { GetScreenData, UpdateScreenData } from './requests';
 
-function updateScreenInput(data){
-  const screenInput = document.getElementById('screenOutput');
+function updateScreenInput(data) {
+  const screenInput = document.getElementById("screenOutput");
   screenInput.textContent = data;
 }
 
 const equalsButton = document.getElementById("equalsButton")
 equalsButton.addEventListener("click", async function(e) {
     const screenData = await GetScreenData()
-    console.log(screenData)
     updateScreenInput(screenData);
 }, false);
 
+const nr1Button = document.getElementById("nr1Button");
+nr1Button.addEventListener("click", async function(e){
+    var screenData = await GetScreenData();
+    screenData = screenData + "1";
+    screenData = await UpdateScreenData(screenData);
+    updateScreenInput(screenData);
+});
+
+const delButton = document.getElementById("delButton");
+delButton.addEventListener("click", async function(e) {
+    var screenData = await GetScreenData();
+    screenData = screenData.slice(0, -1); ;
+    screenData = await UpdateScreenData(screenData);
+    updateScreenInput(screenData);
+})
--- frontend/src/requests.js
+++ frontend/src/requests.js
@@ -13,4 +13,27 @@ async function GetScreenData(){
     });
 };
 
-export { GetScreenData };
+async function UpdateScreenData(screenData){
+  const url = new URL("http://localhost:3001/screen/")
+  const data = {data: screenData}
+  const options = {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(data)
+  }
+  console.log(options)
+  return fetch(url, options)
+    .then((response) => response.json())
+    .then((screenData) => {
+      return screenData.data;
+    })
+    .catch((error) => {
+      console.error("Error updating screen data:", error);
+      throw error
+    });
+
+};
+
+export { GetScreenData, UpdateScreenData };
